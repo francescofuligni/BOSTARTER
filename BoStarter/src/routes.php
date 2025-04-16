d0467e4a5295e148d65e6226c7bcff5d6d994ff8
Francesco <114609462+francescofuligni@users.noreply.github.com>
2025-04-16 17:46:52 +0200

    fix: file index corretto

--- BoStarter/src/routes.php
+++ BoStarter/src/routes.php
@@ -1,15 +1,5 @@
 <?php
 
-define('BASE_PATH', __DIR__);
-require_once __DIR__ . '/models/Router.php';
-
-// Avvia la sessione
-session_start();
-
-// Istanzia il router
-$router = new Router();
-
-// Definizione delle rotte
 $router->get('/', 'views/home.php');
 $router->get('/login', 'views/login.php');
 $router->get('/admin-login', 'views/admin-login.php');
@@ -21,6 +11,3 @@ $router->get('/logout', 'controllers/LogoutController.php');
 $router->post('/login', 'controllers/LoginController.php');
 $router->post('/admin-login', 'controllers/AdminLoginController.php');
 $router->post('/register', 'controllers/RegisterController.php');
-
-// Risoluzione della rotta corrente
-$router->resolve($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
