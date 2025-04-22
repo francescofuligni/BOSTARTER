ae2c8cfe9164401932e72d3c550933bdcb044707
Francesco <114609462+francescofuligni@users.noreply.github.com>
2025-04-22 23:01:57 +0200

    fix: comments and refactoring names

--- BoStarter/src/controllers/LogoutController.php
+++ BoStarter/src/controllers/LogoutController.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/../models/User.php';
 // Avvia la sessione se non è già stata avviata
 if (session_status() == PHP_SESSION_NONE) session_start();
 
-$user = new User(null); // Non serve db per cancellare il token
-$user->logout();
+$userModel = new User(null); // Non serve connessione al db per cancellare il token
+$userModel->logout();
 ?>
