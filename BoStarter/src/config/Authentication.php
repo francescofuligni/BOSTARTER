d33080a3d9070b5b7b174122325c886e6bf73733
Francesco <114609462+francescofuligni@users.noreply.github.com>
2025-04-26 22:15:45 +0200

    feat: update ddl and triggers
    Updated ddl adding profile state and trigger on edit on candidates

--- BoStarter/src/config/Authentication.php
+++ BoStarter/src/config/Authentication.php
@@ -12,8 +12,7 @@ class Authentication
      *
      * @return bool True se il token è valido, false altrimenti.
      */
-    public function validateAuthToken()
-    {
+    public function validateAuthToken() {
         // Avvia la sessione se non è già stata avviata
         if (session_status() == PHP_SESSION_NONE) {
             session_start();
@@ -35,13 +34,9 @@ class Authentication
 
         // Estendi il tempo di scadenza del token
         $_SESSION['token_expiration'] = time() + (60 * 60); // Estendi di 60 minuti
-    
-        // Example logic for token validation
-        if (empty($authToken)) {
-            return false;
-        }
-        // Controlla se il token fornito corrisponde a quello della sessione
-        if ($authToken !== $_SESSION['auth_token']) {
+        $authToken = $_SESSION['auth_token'];
+
+        if (empty($authToken) || $authToken !== $_SESSION['auth_token']) {
             return false;
         }
         return true;    
