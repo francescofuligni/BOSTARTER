51d817f561082be04aa168333e0eac29b99027d9
Francesco <114609462+francescofuligni@users.noreply.github.com>
2025-06-01 09:52:16 +0200

    fix

--- BoStarter/mysql_scripts/events.sql
+++ BoStarter/mysql_scripts/events.sql
@@ -5,7 +5,6 @@ SET GLOBAL event_scheduler = ON;
 -- Evento per chiudere i progetti scaduti (eseguito ogni giorno)
 DROP EVENT IF EXISTS chiudi_progetti_scaduti;
 
---- testare con every 1 minute
 DELIMITER //
 CREATE EVENT chiudi_progetti_scaduti
 ON SCHEDULE EVERY 1 DAY   
